568ad8f9fa3f2c6a687dcd21e260ba562a420e72
play <raunak.mehra17@gmail.com>
2019-09-10 18:12:30 +0530

    fixed some bugs

--- db/mongoose.js
+++ db/mongoose.js
@@ -7,12 +7,7 @@ mongoose.connect('mongodb+srv://raunak:8802945451@delhi-places-5q1f6.mongodb.net
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true
-}).then(() => {
-    console.log("Successfully connected to the database");    
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-});
+})
 
 module.exports = {
     mongoose
