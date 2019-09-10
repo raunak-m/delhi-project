3085b196c301f7a51ce9b0644648ae31e243d1aa
play <raunak.mehra17@gmail.com>
2019-09-10 19:22:22 +0530

    fixed some bugs

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb+srv://raunak:8802945451@delhi-places-5q1f6.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://raunak%3A8802945451@delhi-places-5q1f6.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
