fa6c7bc6f62ecf7cad99fe597870fecf0b5eb272
play <raunak.mehra17@gmail.com>
2019-09-11 21:56:55 +0530

    fixed some bugs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Places = require('./models/places')
 require('./db/mongoose')
 
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 app.get('/places/:id', async (req, res) => {
     try {
